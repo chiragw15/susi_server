4d77570c748f9facad509bb6031cfe61b6eacd83
cc15 <chiragwadhera15@gmail.com>
2017-07-24 21:43:20 +0530

    test

--- kubernetes/travis/deploy.sh
+++ kubernetes/travis/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-export DEPLOY_BRANCH=${DEPLOY_BRANCH:-development}
+export DEPLOY_BRANCH=${DEPLOY_BRANCH:-kubernetes6}
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "fossasia/susi_server" -o  "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "chiragw15/susi_server" -o  "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
     echo "Skip production deployment for a very good reason."
     exit 0
 fi
